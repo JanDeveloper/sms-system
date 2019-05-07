3aa994654e832251a4a90a77d3fcfd8b7a57ed3d
jansojak <sojak@mts.rs>
2019-05-07 17:22:45 +0200

    initialize

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,8 +52,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'phone_number' => ['required', 'string', 'max:11'],
-
         ]);
     }
 
@@ -69,7 +67,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'phone_number' => $data['phone_number'],
         ]);
     }
 }
